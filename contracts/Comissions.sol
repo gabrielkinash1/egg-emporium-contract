0f999e04af47503e26bfd38243536ff92320dfb6
Gabriel "Kinash <gabriel@kinash.xyz>
2022-03-20 11:19:35 -0300

    base smart contract

--- contracts/Comissions.sol
+++ contracts/Comissions.sol
@@ -1,3 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Comissions is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Ownable {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+    
+    string private _defaultBaseURI;
+
+    constructor() ERC721("EE Comissions", "EEGGC") {
+        console.log("Egg Emporium Comissions contract deployed!");
+    }
+
+    function setBaseURI(string calldata newBaseURI) public onlyOwner {
+        _defaultBaseURI = newBaseURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"));
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _defaultBaseURI;
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
